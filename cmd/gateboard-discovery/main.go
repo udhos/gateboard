a2383f404526d273abfa11df0a3cd3fd5fe1122a
udhos <udhos>
2023-01-19 18:42:58 -0300

    Discovery version.

--- cmd/gateboard-discovery/main.go
+++ cmd/gateboard-discovery/main.go
@@ -13,7 +13,7 @@ import (
 	"time"
 )
 
-const version = "0.0.3"
+const version = "0.0.4"
 
 func getVersion(me string) string {
 	return fmt.Sprintf("%s version=%s runtime=%s GOOS=%s GOARCH=%s GOMAXPROCS=%d",
