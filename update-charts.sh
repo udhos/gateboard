4be20c7f39be62078f7df1b331d5ad4b0556a350
udhos <udhos>
2023-02-05 03:36:50 -0300

    Fix chart url.

--- update-charts.sh
+++ update-charts.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# the ./docs dir is published as https://udhos.github.io/miniapi/
+# the ./docs dir is published as https://udhos.github.io/gateboard/
 
 # generate chart package from source
 helm package ./charts/gateboard ./charts/gateboard-discovery -d ./docs
 
 # regenerate the index from existing chart packages
-helm repo index ./docs --url https://udhos.github.io/miniapi/
+helm repo index ./docs --url https://udhos.github.io/gateboard/
 
 echo "#"
 echo "# check that ./docs is fine then:"
