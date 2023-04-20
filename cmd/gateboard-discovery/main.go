e5136bf5d9939542862d8a2947aa714011618c72
udhos <udhos>
2023-04-20 00:10:58 -0300

    Update charts.

--- cmd/gateboard-discovery/main.go
+++ cmd/gateboard-discovery/main.go
@@ -13,7 +13,7 @@ import (
 	"time"
 )
 
-const version = "0.0.17"
+const version = "0.1.0"
 
 func getVersion(me string) string {
 	return fmt.Sprintf("%s version=%s runtime=%s GOOS=%s GOARCH=%s GOMAXPROCS=%d",
--- cmd/gateboard/main.go
+++ cmd/gateboard/main.go
@@ -26,7 +26,7 @@ import (
 	"github.com/udhos/gateboard/tracing"
 )
 
-const version = "0.0.23"
+const version = "0.1.0"
 
 type application struct {
 	serverMain    *serverGin
