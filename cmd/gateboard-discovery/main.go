5f0a3d51c48f02a47d54d6aaee2f18ecec31f136
udhos <udhos>
2023-08-16 12:23:26 -0300

    New chart using release name as deployment container name.

--- cmd/gateboard-discovery/main.go
+++ cmd/gateboard-discovery/main.go
@@ -19,7 +19,7 @@ import (
 	"go.opentelemetry.io/otel/trace"
 )
 
-const version = "1.6.5"
+const version = "1.6.6"
 
 func main() {
 
--- docker/build-discovery.sh
+++ docker/build-discovery.sh
@@ -9,3 +9,5 @@ docker build \
     -t udhos/gateboard-discovery:latest \
     -t udhos/gateboard-discovery:$version \
     -f docker/Dockerfile.discovery .
+
+echo "push: docker push udhos/gateboard-discovery:$version; docker push udhos/gateboard-discovery:latest"
