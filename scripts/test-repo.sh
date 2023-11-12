c10aa54b136f6bc184c5c5a64a17fdbf2ba0a7e0
udhos <udhos>
2023-11-12 15:32:19 -0300

    Typo.

--- scripts/test-repo.sh
+++ scripts/test-repo.sh
@@ -11,6 +11,6 @@ read i
 export TEST_REPO_MONGO=true ;# enable mongodb tests
 export TEST_REPO_DYNAMO=true ;# enable dynamodb tests
 export TEST_REPO_REDIS=true ;# enable redis tests
-export TEST_REPO_S3=pub_bucket_name_here ;# enable S3 tests
+export TEST_REPO_S3=put_bucket_name_here ;# enable S3 tests
 
 go test -count=1 -run TestRepository ./cmd/gateboard
